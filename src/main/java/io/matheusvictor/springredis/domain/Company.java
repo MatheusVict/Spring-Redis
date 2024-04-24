ced375cc7d7ad6b5b5bdfcc5ad0cc07f273d8bf6
MatheusVict <matheusvictorhenrique@gmail.com>
2024-04-23 22:38:13 -0300

    chore: adjust populate entity command

--- src/main/java/io/matheusvictor/springredis/domain/Company.java
+++ src/main/java/io/matheusvictor/springredis/domain/Company.java
@@ -14,7 +14,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Company {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS empresa
+CREATE TABLE IF NOT EXISTS company
 (
     id
     INT
     PRIMARY
     KEY,
-    nome
+    name
     varchar
 (
     20
@@ -17,10 +17,10 @@ DECLARE
     cont INT := 1;
     registros INT := 0;
 BEGIN
-    SELECT COUNT(*) INTO registros FROM empresa;
+    SELECT COUNT(*) INTO registros FROM company;
     IF registros = 0 THEN
         FOR cont IN 1..300000 LOOP
-            INSERT INTO empresa (id, nome)
+            INSERT INTO company (id, name)
             VALUES (cont, CONCAT(''Empresa '', cont));
         END LOOP;
     END IF;
