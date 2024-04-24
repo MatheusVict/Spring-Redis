a9545938821155c592ade16f98264d90706f1d0a
MatheusVict <matheusvictorhenrique@gmail.com>
2024-04-23 22:14:26 -0300

    feat: add company entity
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/main/java/io/matheusvictor/springredis/domain/Company.java
@@ -0,0 +1,21 @@
+package io.matheusvictor.springredis.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "company")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
