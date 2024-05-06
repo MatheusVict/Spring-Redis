0538453eb5326e5b30357ad259dcb848c99b740a
MatheusVict <matheusvictorhenrique@gmail.com>
2024-05-06 13:39:46 -0300

    feat: add serializable

--- src/main/java/io/matheusvictor/springredis/cloud/IbgeResponse.java
+++ src/main/java/io/matheusvictor/springredis/cloud/IbgeResponse.java
@@ -2,5 +2,6 @@ package io.matheusvictor.springredis.cloud;
 
 import java.io.Serializable;
 
+// needs serializable to won't receive cache errors in cache
 public record IbgeResponse(int id, String nome) implements Serializable {
 }
